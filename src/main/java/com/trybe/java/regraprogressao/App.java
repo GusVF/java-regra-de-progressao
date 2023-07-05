c129457f2b5b92e63d8c61b7cb6f06ce8b143ab3
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-07-05 18:47:44 -0300

    trying to pass evaluator

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * Class App.
  */
@@ -11,6 +13,43 @@ public class App {
    */
 
   public static void main(String[] args) {
-    CadastroDeAtividades.cadastraAtividades();
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+
+    int activityCount = scanner.nextInt();
+    String[] activityName = new String[activityCount];
+    int[] activityWeight = new int[activityCount];
+    int[] activityGrade = new int[activityCount];
+
+    for (int i = 0; i < activityCount; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      scanner.nextLine();
+      activityName[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      activityWeight[i] = scanner.nextInt();
+
+      System.out.println("Digite a nota obtida para " + activityName[i] + " :");
+      activityGrade[i] = scanner.nextInt();
+
+    }
+    double totalScore = 0.0;
+    for (int i = 0; i < activityCount; i++) {
+      double weightPercentege = (double) activityWeight[i] / 100;
+      double activityScore = activityGrade[i] * weightPercentege;
+      totalScore += activityScore;
+    }
+
+    if (totalScore >= 85) {
+      System.out.println("Parabéns! Você alcançou " + totalScore
+          + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + totalScore
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
+    scanner.close();
   }
 }
